6d2d9072b017235ef14ed7863544a46c25bb7fac
Jane-Mar <106934020+Jane-Mar@users.noreply.github.com>
2022-07-22 23:29:07 +0100

    added api call for forecast

--- src/ForecastWrapper.js
+++ src/ForecastWrapper.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherImage from "./WeatherImage";
+import axios from "axios";
 
 import "./ForecastWrapper.css";
 
 export default function ForecastWrapper(props) {
-  return (
-    <div className="ForecastWrapper">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Thu</div>
-          <div className="forecast-image text-center">
-            <WeatherImage code={props.info.imageCode} size={50} />
-          </div>
-          <div className="forecast-temp">
-            <span className="forecast-temp-day">20 °</span> /{" "}
-            <span className="forecast-temp-night opacity-75"> 10°</span>
+  const [ready, setReady] = useState(false);
+
+  function hendleResponse() {
+    setReady(true);
+  }
+
+  function getWeather() {
+    let lat = props.info.coordinates.lat;
+    let lon = props.info.coordinates.lon;
+    let apiKey = "fff7fbe34f6b248c3ba3dfbbe41d297f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(hendleResponse);
+  }
+
+  if (ready) {
+    return (
+      <div className="ForecastWrapper">
+        <div className="row">
+          <div className="col">
+            <div className="forecast-day">Thu</div>
+            <div className="forecast-image text-center">
+              <WeatherImage code={props.info.imageCode} size={50} />
+            </div>
+            <div className="forecast-temp">
+              <span className="forecast-temp-day"> °</span> /{" "}
+              <span className="forecast-temp-night opacity-75"> 10°</span>
+            </div>
           </div>
         </div>
-        `;
       </div>
-    </div>
-  );
+    );
+  } else {
+    getWeather();
+  }
 }
